eee2428f121f27958c26686ff298f7bfbd960f63
molghi <54518k@gmail.com>
2025-03-07 06:26:26 +0300

    fetching assets, building data set

--- src/components/data.js
+++ src/components/data.js
@@ -2,10 +2,61 @@ import brownNoiseAudio from "../audio/brown-noise.mp3";
 import brownNoiseJpg from "../images/white-noise.jpg";
 import brownNoiseGif from "../images/white-noise.gif";
 
-import spaceshipSleepingQuarters from "../audio/spaceship-sleeping-quarters.mp3";
-import spaceshipSleepingQuartersJpg from "../images/spaceship-sleeping-quarters.png";
+import spaceshipSleepingQuartersAudio from "../audio/spaceship-sleeping-quarters.mp3";
+import spaceshipSleepingQuartersJpg from "../images/spaceship-sleeping-quarters.jpg";
+import spaceshipSleepingQuartersGif from "../images/spaceship-sleeping-quarters.gif";
 
-import forestNight2 from "../audio/forest-night-2.mp3";
+import forestDayAudio from "../audio/forest-day.mp3";
+import forestDayJpg from "../images/forest-day.jpg";
+import forestDayGif from "../images/forest-day.gif";
+
+import forestNightAudio from "../audio/forest-night.mp3";
+import forestNightJpg from "../images/night.jpg";
+import forestNightGif from "../images/night.gif";
+
+import rainAudio from "../audio/rain.mp3";
+import rainJpg from "../images/rain.jpg";
+import rainGif from "../images/rain.gif";
+
+import thunderAudio from "../audio/rain-thunder.mp3";
+import thunderJpg from "../images/thunder.jpg";
+import thunderGif from "../images/thunder.gif";
+
+import trainAudio from "../audio/train.mp3";
+import trainJpg from "../images/train.jpg";
+import trainGif from "../images/train.gif";
+
+import fireplaceAudio from "../audio/fireplace.mp3";
+import fireplaceJpg from "../images/fireplace.jpg";
+import fireplaceGif from "../images/fireplace.gif";
+
+import cavesAudio from "../audio/caves-waterfall.mp3";
+import cavesJpg from "../images/caves.jpg";
+import cavesGif from "../images/caves.gif";
+
+import winterAudio from "../audio/winter-snowfall.mp3";
+import winterJpg from "../images/snow.jpg";
+import winterGif from "../images/snow.gif";
+
+import aeroplaneAudio from "../audio/aeroplane-interior.mp3";
+import aeroplaneJpg from "../images/airplane.jpg";
+import aeroplaneGif from "../images/airplane.gif";
+
+import airconAudio from "../audio/air-conditioner.mp3";
+import airconJpg from "../images/aircon.jpg";
+import airconGif from "../images/aircon.jpg";
+
+import underwaterAudio from "../audio/underwater.mp3";
+import underwaterJpg from "../images/flashlights.jpg";
+import underwaterGif from "../images/flashlights.gif";
+
+import serverRoomAudio from "../audio/server-room.mp3";
+import serverRoomJpg from "../images/server-room.jpg";
+import serverRoomGif from "../images/server-room.gif";
+
+import oldGameAudio from "../audio/old-game.mp3";
+import oldGameJpg from "../images/filch.jpg";
+import oldGameGif from "../images/filch.gif";
 
 // ================================================================================================
 
@@ -20,16 +71,100 @@ const data = [
     {
         id: 2,
         name: "Spaceship Sleeping Quarters",
-        audioPath: spaceshipSleepingQuarters,
+        audioPath: spaceshipSleepingQuartersAudio,
         imgPathJpg: spaceshipSleepingQuartersJpg,
-        imgPathGif: spaceshipSleepingQuartersJpg,
+        imgPathGif: spaceshipSleepingQuartersGif,
     },
     {
-        id: 2,
-        name: "Forest At Night (2)",
-        audioPath: forestNight2,
-        imgPathJpg: spaceshipSleepingQuartersJpg,
-        imgPathGif: spaceshipSleepingQuartersJpg,
+        id: 3,
+        name: "Forest by Day",
+        audioPath: forestDayAudio,
+        imgPathJpg: forestDayJpg,
+        imgPathGif: forestDayGif,
+    },
+    {
+        id: 4,
+        name: "Forest at Night",
+        audioPath: forestNightAudio,
+        imgPathJpg: forestNightJpg,
+        imgPathGif: forestNightGif,
+    },
+    {
+        id: 5,
+        name: "Rainy Weather",
+        audioPath: rainAudio,
+        imgPathJpg: rainJpg,
+        imgPathGif: rainGif,
+    },
+    {
+        id: 6,
+        name: "Rain and Thunder",
+        audioPath: thunderAudio,
+        imgPathJpg: thunderJpg,
+        imgPathGif: thunderGif,
+    },
+    {
+        id: 7,
+        name: "Train Carriage",
+        audioPath: trainAudio,
+        imgPathJpg: trainJpg,
+        imgPathGif: trainGif,
+    },
+    {
+        id: 8,
+        name: "Cosy Fireplace",
+        audioPath: fireplaceAudio,
+        imgPathJpg: fireplaceJpg,
+        imgPathGif: fireplaceGif,
+    },
+    {
+        id: 9,
+        name: "Caves by Waterfall",
+        audioPath: cavesAudio,
+        imgPathJpg: cavesJpg,
+        imgPathGif: cavesGif,
+    },
+    {
+        id: 10,
+        name: "Winter Snowfall",
+        audioPath: winterAudio,
+        imgPathJpg: winterJpg,
+        imgPathGif: winterGif,
+    },
+    {
+        id: 11,
+        name: "Aeroplane Interior",
+        audioPath: aeroplaneAudio,
+        imgPathJpg: aeroplaneJpg,
+        imgPathGif: aeroplaneGif,
+    },
+    {
+        id: 12,
+        name: "Air Conditioner",
+        audioPath: airconAudio,
+        imgPathJpg: airconJpg,
+        imgPathGif: airconGif,
+    },
+    {
+        id: 13,
+        name: "Deep Underwater",
+        audioPath: underwaterAudio,
+        imgPathJpg: underwaterJpg,
+        imgPathGif: underwaterGif,
+    },
+    {
+        id: 14,
+        name: "Old Game (Night Sneak)",
+        audioPath: oldGameAudio,
+        imgPathJpg: oldGameJpg,
+        imgPathGif: oldGameGif,
+    },
+    {
+        id: 15,
+        name: "Server Room",
+        audioPath: serverRoomAudio,
+        imgPathJpg: serverRoomJpg,
+        imgPathGif: serverRoomGif,
     },
 ];
 
